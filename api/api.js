7817956766654b515e1769084668fa4c7a4f21c3
Paras-mourya <parasmourya288@gmail.com>
2025-08-25 21:06:00 +0530

    firstcommit

--- api/api.js
+++ api/api.js
@@ -1,8 +1,7 @@
-// api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000", // correct port
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
 });
 
